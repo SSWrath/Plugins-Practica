587e85c811364fecb3621b5005cbe8b2541afeed
jcedeno-m1 <jcedeno@noobsters.net>
2021-10-19 14:23:47 -0400

    fix

--- src/main/java/me/aleiv/core/paper/listeners/Ninja.java
+++ src/main/java/me/aleiv/core/paper/listeners/Ninja.java
@@ -7,43 +7,37 @@ import org.bukkit.potion.PotionEffectType;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import me.aleiv.core.paper.Core;
 
 @Data
 @AllArgsConstructor(staticName = "of")
 public class Ninja {
     Player player;
- 
-    public void removeInvisibility() {
-        player.getActivePotionEffects()
-                .stream()
-                .filter((effect) -> (effect.getType() == PotionEffectType.INVISIBILITY))
-                .forEach(this::removePotionEffect);
-    }
 
-    private void removePotionEffect(PotionEffect effect){
-        player.removePotionEffect(effect.getType());
+    public void removeInvisibility() {
+        player.removePotionEffect(PotionEffectType.INVISIBILITY);
+        player.sendMessage(Core.getMiniMessage().parse("<rainbow>Ninja mode disable."));
     }
 
     public void giveInvisibility() {
-        player.addPotionEffect(PotionEffectType.INVISIBILITY.createEffect(20 * 5, 0));
+        player.sendMessage(Core.getMiniMessage().parse("<rainbow>Ninja mode enabled."));
+        player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 9999 * 20, 0, false, false));
     }
 
     public boolean isPlayerHidden() {
-
         return player.hasPotionEffect(PotionEffectType.INVISIBILITY);
-
     }
 
     public boolean isPlayerShiftClick(PlayerInteractEvent e) {
         var action = e.getAction();
         switch (action) {
-            case RIGHT_CLICK_BLOCK:
-            case LEFT_CLICK_BLOCK:
-            case RIGHT_CLICK_AIR:
-            case LEFT_CLICK_AIR:
-                return player.isSneaking();
-            default:
-                return false;
+        case RIGHT_CLICK_BLOCK:
+        case LEFT_CLICK_BLOCK:
+        case RIGHT_CLICK_AIR:
+        case LEFT_CLICK_AIR:
+            return player.isSneaking();
+        default:
+            return false;
         }
     }
 
